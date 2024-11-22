8d0e8edc4b149b739a215f6b207823a3ef12d2b5
PUMCZACZ <pawel.cebernik02@gmail.com>
2024-11-22 09:46:06 +0100

    WT-781 code improvments

--- src/Subscriber/OrderSubscriber.php
+++ src/Subscriber/OrderSubscriber.php
@@ -13,7 +13,6 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class OrderSubscriber implements EventSubscriberInterface
 {
-    const string EDRONE_COOKIE_NAME = 'crehlerEdroneCrm.cookie.edroneName';
     public function __construct(
         private readonly EdroneService $edroneService,
         private readonly EdroneCookieProvider $edroneCookieProvider
@@ -47,21 +46,5 @@ class OrderSubscriber implements EventSubscriberInterface
 
             $this->edroneService->orderChanged($payload['id'], $payload['stateId'], $event->getContext());
         }
-    }
-
-    /**
-     * Check if user accept Edrone cookies consent
-     *
-     * @return bool
-     */
-    private function isCookieConsentAccepted(): bool
-    {
-        foreach ($this->edroneCookieProvider->getCookieGroups() as $cookie) {
-            if ($cookie['snippet_name'] === self::EDRONE_COOKIE_NAME) {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
